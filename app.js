e46f32f73d59fa6af6ae516f9f7f72d77bf9a43f
Eric Bayasgalan <ewiz1020@gmail.com>
2021-04-23 18:44:43 -0500

    Implement functionality

---
+++ app.js
@@ -0,0 +1,31 @@
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+const kelvinInput = document.querySelector('#kelvin > input');
+
+const celciusToFahrenheitAndKelvin = () => {
+  const cTemp = parseFloat(celciusInput.value);
+  const fTemp = (cTemp * (9/5)) + 32;
+  const kTemp = cTemp + 273.15;
+  fahrenheitInput.value = fTemp;
+  kelvinInput.value = kTemp;
+}
+
+const fahrenheitToCelciusAndKelvin = () => {
+  const fTemp = parseFloat(fahrenheitInput.value);
+  const cTemp = (fTemp - 32) * (5/9);
+  const kTemp = (fTemp + 459.67) * (5/9);
+  celciusInput.value = cTemp;
+  kelvinInput.value = kTemp;
+}
+
+const kelvinToCelciusAndFahrenheit = () => {
+  const kTemp = parseFloat(kelvinInput.value);
+  const cTemp = kTemp - 273.15;
+  const fTemp = 9/5 * (kTemp - 273) + 32;
+  celciusInput.value = cTemp;
+  fahrenheitInput.value = fTemp;
+}
+
+celciusInput.addEventListener('input', celciusToFahrenheitAndKelvin);
+fahrenheitInput.addEventListener('input', fahrenheitToCelciusAndKelvin);
+kelvinInput.addEventListener('input', kelvinToCelciusAndFahrenheit);
